fee23e39ba2bf898f9b079cbea6e9daacf00b734
Jaquee <jaquee.monero@gmail.com>
2017-08-04 23:31:33 +0200

    walletAPI: add lightwallet login() and importWalletRequest()

--- src/wallet/api/wallet.h
+++ src/wallet/api/wallet.h
@@ -128,6 +128,8 @@ public:
     virtual void startRefresh();
     virtual void pauseRefresh();
     virtual bool parse_uri(const std::string &uri, std::string &address, std::string &payment_id, uint64_t &amount, std::string &tx_description, std::string &recipient_name, std::vector<std::string> &unknown_parameters, std::string &error);
+    virtual bool lightWalletLogin(bool &isNewWallet) const;
+    virtual bool lightWalletImportWalletRequest(std::string &payment_id, uint64_t &fee, bool &new_request, bool &request_fulfilled, std::string &payment_address, std::string &status);
 
 private:
     void clearStatus() const;
